03f4cbf8d07363a5679eaa2aa643e21cd8f0c461
YOKKESH K <yokkesh2002hackerone@gmail.com>
2023-07-12 18:49:24 +0530

    five

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -4,8 +4,8 @@ const about=()=>{
   return(
     <div className='ui raised very padded text container segment'>
     <h1>This Is My About Us Page</h1>
-    <p>The desert storm was riding in its strength; the travellers lay beneath
-    the mastery of the fell simoom.
+    <p>My Name is yokkesh, im a web developer and  an web designer, visit my
+    website for more information.
     </p>
     </div>
 
