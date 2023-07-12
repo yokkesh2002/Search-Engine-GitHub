ca1f26fcba4acc65c1b87f57f1b0ba7812cd989e
YOKKESH K <yokkesh2002hackerone@gmail.com>
2023-07-12 18:38:34 +0530

    three

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -4,7 +4,7 @@ const contact=()=>{
   return(
     <div className='ui raised very padded text container segment'>
     <h1>This Is My Contact Us Page</h1>
-    <p>The desert storm was riding in its strength; the travellers lay beneath
+    <p>My Name is Yokkesh.The desert storm was riding in its strength; the travellers lay beneath
     the mastery of the fell simoom.
     </p>
     </div>
